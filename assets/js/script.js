851503d404189ae25591d800bbe9d445f1aabc0d
Chelsea-Luevano <120981491+ChelseaLuevano@users.noreply.github.com>
2023-03-01 21:27:23 -0500

    added ids and classes to html, changed the city list to include buttons, started first API function in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,5 +6,22 @@ let state;
 let country;
 let queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
 
+// Function to call weather API to determine city weather
 
-fetch(queryURL);
+function callWeatherAPI () {
+    
+
+    fetch(queryURL)
+    .then(function(response){
+            if (response.ok) {
+                response.json().then
+            }
+            else {
+                alert('Unable to connect to Weather API');
+            }
+        }
+        )
+}
+
+callWeatherAPI();
+
