15f78822db4b96241f06f96b332406138bddc3fc
Chelsea-Luevano <120981491+ChelseaLuevano@users.noreply.github.com>
2023-03-01 20:55:24 -0500

    added aside html content

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 // Variables
 let aPIKey = eec8d3f247c70019f179e96be36c660d; 
 let city; 
+let lastSearchedCity = document. querySelector('input'). value;
 let state;
 let country;
 let queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
