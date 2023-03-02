9eafed0f6a33234cee03f79d704788a14950c4c9
Chelsea-Luevano <120981491+ChelseaLuevano@users.noreply.github.com>
2023-03-01 21:38:31 -0500

    added some more ids and defined variables in js for html elements through query selector

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,18 @@
-// Variables
+// API Key Variable
 let aPIKey = eec8d3f247c70019f179e96be36c660d; 
-let city; 
-let lastSearchedCity = document. querySelector('input'). value;
+
+// Query Selector Variables
+let cityFormEl = document.querySelector('#city-form');
+let cityButtonsEl = document.querySelector('#city-btn');
+let cityInputEl = document.querySelector('#city');
+let cityWeatherEl = document.querySelector('#city-weather');
+let forecastEl = document.querySelector('#forecast');
+let lastSearchedCityEl = document.querySelector('input').value;
+
+// Data Variables
 let state;
 let country;
-let queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+let queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityInputEl + "&appid=" + APIKey;
 
 // Function to call weather API to determine city weather
 
