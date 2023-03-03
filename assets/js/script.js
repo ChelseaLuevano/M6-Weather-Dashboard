c3b20373753538765939986761c890f1c4450854
Chelsea-Luevano <120981491+ChelseaLuevano@users.noreply.github.com>
2023-03-03 10:14:22 -0500

    added to weather api function, created formSearchHandler function, and updated search button event listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,9 @@
 // API Key Variable
-let aPIKey = eec8d3f247c70019f179e96be36c660d; 
+let apiKey = "eec8d3f247c70019f179e96be36c660d"; 
 
 // Query Selector Variables
 let cityFormEl = document.querySelector('#city-form');
+let searchButtonEl = document.querySelector('#searchbtn');
 let cityButtonsEl = document.querySelector('#city-btn');
 let cityInputEl = document.querySelector('#city');
 let cityWeatherEl = document.querySelector('#city-weather');
@@ -12,24 +13,50 @@ let lastSearchedCityEl = document.querySelector('input').value;
 // Data Variables
 let state;
 let country;
-let queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityInputEl + "&appid=" + APIKey;
+let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityInputEl.value + "&appid=" + apiKey;
+
+// function
+let formSearchHandler = function (event) {
+    event.preventDefault(); 
+
+    // Defining cityName input box value entered by user and removing any whitespace
+    let cityName = cityInputEl.value.trim();
+
+    WeatherAPI();
+
+    if (cityName) {
+        console.log("wowww I did it")
+    } else {
+    alert("Wow I didn't do it");
+    } 
+}
 
 // Function to call weather API to determine city weather
-
-function callWeatherAPI () {
-    
-
+function WeatherAPI () {
     fetch(queryURL)
     .then(function(response){
             if (response.ok) {
-                response.json().then
+                // need to look at other examples to make sure the .then below is correct as instructor said this isn't happening
+                response.json().then(function (data) {
+                    console.log(data);
+                  });
             }
             else {
                 alert('Unable to connect to Weather API');
             }
         }
-        )
+        ) 
 }
 
-callWeatherAPI();
+
+
+// When the search button is clicked, then the Weather API function 
+searchButtonEl.addEventListener('submit',formSearchHandler);
+
+// save cityInputEl Value & call Weather API function
+// function saveCity() {
+//     cityInputEl = cityInputEl.value.trim();
+//     WeatherAPI();
+// }
+
 
